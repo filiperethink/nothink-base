43ccf8da982188bef86fe3e65bc432897b5cb57b
Filipe Prado - Rethink <filipe.prado@rethink.dev>
2022-04-28 16:31:04 -0300

    chore: adding css reset and fetching notes via firebase

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 import Firebase, { db } from "./firebaseConfig.js";
+const listNotes = document.getElementById("list-notes");
+const isLoadingElement = document.getElementById("isLoading");
 
-Firebase.getDocs(Firebase.collection(db, "notes")).then((result) => {
-  result.forEach((doc) => {
-    console.log(doc.data());
+async function getNotes() {
+  const notes = await Firebase.getDocs(Firebase.collection(db, "notes"));
+  return notes.docs.map((doc) => doc.data());
+}
+
+function renderNotes(notes) {
+  notes.forEach((note) => {
+    let tmp = document.createElement("li");
+    tmp.innerHTML = note.title;
+    listNotes.appendChild(tmp);
   });
-});
+}
+
+async function initApp() {
+  isLoadingElement.innerHTML = "Loading...";
+  const notes = await getNotes();
+  document.body.removeChild(isLoadingElement);
+  renderNotes(notes);
+}
+
+initApp();
