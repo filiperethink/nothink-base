16752db1fb34f9092d5a66ba76c0220ff14f22cc
Filipe Prado - Rethink <filipe.prado@rethink.dev>
2022-04-29 11:09:53 -0300

    chore: adding click to list itens

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 import Firebase, { db } from "./firebaseConfig.js";
 const listNotes = document.getElementById("list-notes");
 const isLoadingElement = document.getElementById("isLoading");
+const btnCreateElement = document.getElementById("btnCreate");
 
 async function getNotes() {
   const notes = await Firebase.getDocs(Firebase.collection(db, "notes"));
-  return notes.docs.map((doc) => doc.data());
+
+  return notes.docs.map((doc) => {
+    return {
+      id: doc.id,
+      ...doc.data(),
+    };
+  });
 }
 
 function renderNotes(notes) {
   notes.forEach((note) => {
-    let tmp = document.createElement("li");
-    tmp.innerHTML = note.title;
-    listNotes.appendChild(tmp);
+    listNotes.innerHTML += `
+      <li class="card" id="cardItem" tagName="${note.id}">
+        <h4 class="card-title">${note.title}</h4>
+        <p class="card-description">${note.description}</p>
+      </li>
+    `;
+  });
+}
+
+function addEventsToItens() {
+  document.querySelectorAll("#cardItem").forEach((note) => {
+    note.addEventListener("click", () => {
+      document
+        .querySelectorAll("#cardItem")
+        .forEach((note) => (note.classList = "card"));
+      if (note.classList.contains("card-selected")) {
+        note.classList = "card";
+      } else {
+        note.classList += " card-selected";
+      }
+    });
+    note.classList = "card";
   });
 }
 
@@ -20,6 +46,7 @@ async function initApp() {
   const notes = await getNotes();
   document.body.removeChild(isLoadingElement);
   renderNotes(notes);
+  addEventsToItens();
 }
 
 initApp();
